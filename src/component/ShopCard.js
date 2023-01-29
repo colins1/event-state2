ec71af76cd096bca32e9b12dda54070d6c170c9a
colins1 <maxrozhkov1@gmail.com>
2023-01-30 00:17:23 +0300

    add

--- src/component/ShopCard.js
+++ src/component/ShopCard.js
@@ -4,7 +4,17 @@ import Items from './Items';
 
 function ListView (props) {
     return (
-        <div className='poductSectorCard'><h2>{props.products}</h2></div>
+        <div className='poductSectorCard'>
+            <img src={props.products[0].img}/>
+            <div>
+                <h2>{props.products[0].name}</h2>
+                <p>{props.products[0].color}</p>
+            </div>
+            <div>
+                <p>{props.products[0].price}</p>
+                <button>ADD TO CART</button>
+            </div>
+        </div>
     )
 }
 
--- src/component/ShopItem.js
+++ src/component/ShopItem.js
@@ -4,7 +4,19 @@ import Items from './Items';
 
 function ListView (props) {
     return (
-        <div className='poductSectorList'><h2>{props.products}</h2></div>
+        <div>
+            {props.products.map( (product) => {<div className='poductSectorList'>
+            <img src={product.img}/>
+            <div>
+                <h2>{product.name}</h2>
+                <p>{product.color}</p>
+            </div>
+            <div>
+                <p>{product.price}</p>
+                <button>ADD TO CART</button>
+            </div>
+        </div>})}
+        </div>
     )
 }
 
