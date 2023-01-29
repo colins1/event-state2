d1030d3fc6b5b655ccaf25847a05d6586186fb3c
colins1 <maxrozhkov1@gmail.com>
2023-01-29 21:42:20 +0300

    add

--- src/component/Store.js
+++ src/component/Store.js
@@ -1,6 +1,6 @@
 import '../App.css';
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import IconSwitch from './IconSwitch';
 import CardsView from './CardsView';
 import ListView from './ListView';
@@ -14,15 +14,15 @@ class Store extends React.Component {
         this.state = {
             icon: "view_list"
         }
-        let {products} = Items;
+        let {products} = Items
         this.products = products;
     }
 
     render() {
-        retuen (
+        return (
             <div>
                 <IconSwitch icon={this.state.icon} onSwitch={this.state.icon == "view_list" ? this.setState({icon: "view_module"}) : this.setState({icon: "view_list"})} />
-                {this.state.icon === "view_module" ? <CardsView cards={<ShopCard products={products} />} /> : <ListView items={<ShopItem products={products} />} />}
+                {this.state.icon === "view_module" ? <CardsView cards={<ShopCard products={this.products} />} /> : <ListView items={<ShopItem products={this.products} />} />}
             </div>
         )
     }
