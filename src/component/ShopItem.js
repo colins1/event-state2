ff86805b689bd6b27aa344552f4b2a185c6e517a
colins1 <maxrozhkov1@gmail.com>
2023-01-30 00:56:29 +0300

    add

--- src/component/ShopItem.js
+++ src/component/ShopItem.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Items from './Items';
+//import PropTypes from 'prop-types';
 
 function ListView (props) {
+    const {products} = props
+    let arrey =  products.map((product, i) =>
+        <div key={i} className='poductSectorList'>
+        <img src={product.img}/>
+        <div className="title">
+            <h2>{product.name}</h2>
+            <p>{product.color}</p>
+        </div>
+        <div className="futer">
+            <p>{product.price}</p>
+            <button>ADD TO CART</button>
+        </div>
+    </div>
+   )
+
     return (
         <div>
-            {props.products.map( (product) => {<div className='poductSectorList'>
-            <img src={product.img}/>
-            <div>
-                <h2>{product.name}</h2>
-                <p>{product.color}</p>
-            </div>
-            <div>
-                <p>{product.price}</p>
-                <button>ADD TO CART</button>
-            </div>
-        </div>})}
+            {arrey}
         </div>
     )
 }
