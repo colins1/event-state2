8b6ca311da4787736f3b3a22bf90241f56450a2a
colins1 <maxrozhkov1@gmail.com>
2023-01-29 20:59:57 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import React from 'react';
 import PropTypes from 'prop-types';
-import Items from './Items';
+import {products} from './Items';
 import Store from './Store';
 
 function App() {
-  let {products} = Items
-
   return (
     <Store products={products}/>
   );
